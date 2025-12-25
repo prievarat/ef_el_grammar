6a6cd5bf1e0930123f3241c76076e626ab07574a
prievarat <136087908+prievarat@users.noreply.github.com>
2025-12-25 18:52:38 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+// Filename: data.js
+// This file acts as the "Catalog" for your Grammar Mastery Engine.
+const GRAMMAR_UNITS = [
+    { id: "reg_past", title: "Past Simple - Regular", file: "units/regular_past.js" },
+    { id: "irreg_past", title: "Past Simple - Irregular", file: "units/irregular_past.js" },
+    { id: "past_q_neg", title: "Past - Questions & Negations", file: "units/past_q_neg.js" },
+    { id: "pres_vs_cont", title: "Pres. Simple vs. Continuous", file: "units/present_simple_cont.js" },
+    { id: "there_is_are", title: "There is / There are", file: "units/there_is_are.js" },
+    { id: "some_any", title: "Some / Any / No", file: "units/some_any.js" },
+    { id: "comp_adj", title: "Better / More (Comparatives)", file: "units/comparatives.js" },
+    { id: "sup_adj", title: "The Best / The Most (Superlatives)", file: "units/superlatives.js" },
+    { id: "articles_the", title: "The (Articles)", file: "units/articles.js" },
+    { id: "pres_perf", title: "Present Perfect", file: "units/present_perfect.js" }
+];
